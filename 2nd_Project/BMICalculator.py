da5d84b5b3c3f8c65c78e66dc06e0ae6749d3ac0
sanjeevreddy5797 <sanjeevreddy5797@gmail.com>
2026-09-09 05:31:19 +0530

    Adding Intrepretations to BMI calculator

--- 2nd_Project/BMICalculator.py
+++ 2nd_Project/BMICalculator.py
@@ -1,6 +1,11 @@
-height = input("Enter you height in Meters: \n")
-weight = input("Enter your weight in KiloGrams: \n")
+height = float(input("Enter you height in Meters: \n"))
+weight = int(input("Enter your weight in KiloGrams: \n"))
 
 bmi = weight / (height**2)
 
-print(f"Your BMI is {bmi}.")
+if (bmi < 18.5):
+    print("Your are Underweight.")
+elif (bmi >= 18.5 and bmi<25):
+    print("You are normal weight.")
+else:
+    print("You are overweight.")  
